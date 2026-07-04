34836888d75685c64b1b4a70f38d5fa6b13475b1
lawrenetalkss <lawrene00@gmail.com>
2026-07-04 19:01:21 +0100

    updated

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get the last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
